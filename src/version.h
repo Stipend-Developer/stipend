2e55401b6f16b72e092e32a629f8f999c95ca3f2
Stipend-Developer <nicksr987@gmail.com>
2018-05-22 16:02:13 -0400

    Update Protocol

--- src/version.h
+++ src/version.h
@@ -30,14 +30,14 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 11002;
+static const int PROTOCOL_VERSION = 11003;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 11001;
-static const int MIN_PEER_PROTO_VERSION_FORK1 = 11002;
+static const int MIN_PEER_PROTO_VERSION_FORK1 = 11003;
 
 // minimum peer version accepted by DarkSendPool
 static const int MIN_POOL_PEER_PROTO_VERSION = 11001;
@@ -57,7 +57,7 @@ static const int CADDR_TIME_VERSION = 11000;
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 11000;
 static const int NOBLKS_VERSION_END = 11001;
-static const int NOBLKS_VERSION_END_FORK1 = 11002;
+static const int NOBLKS_VERSION_END_FORK1 = 11003;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 11000;
