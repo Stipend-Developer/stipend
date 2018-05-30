d2f010f2a05fd1607f1a5876abb76baa034d7be5
Stipend-Developer <nicksr987@gmail.com>
2018-05-30 18:11:29 -0400

    Final Protocol Updates

--- src/version.h
+++ src/version.h
@@ -40,14 +40,14 @@ static const int MIN_PEER_PROTO_VERSION = 11003;
 static const int MIN_PEER_PROTO_VERSION_FORK1 = 11004;
 
 // minimum peer version accepted by DarkSendPool
-static const int MIN_POOL_PEER_PROTO_VERSION = 11004;
+static const int MIN_POOL_PEER_PROTO_VERSION = 11003;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 11004;
+static const int MIN_INSTANTX_PROTO_VERSION = 11003;
 
 //! minimum peer version that can receive masternode payments
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 11004;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 11003;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
