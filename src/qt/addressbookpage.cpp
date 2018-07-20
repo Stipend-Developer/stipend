ebdc79a7e0568c537a89dd5cf0aa7c28c91fd6e4
Stipend-Developer <nicksr987@gmail.com>
2018-07-20 16:08:52 -0400

    Finalize addressbookpage.ui for wallet redesign

--- src/qt/addressbookpage.cpp
+++ src/qt/addressbookpage.cpp
@@ -51,7 +51,6 @@ AddressBookPage::AddressBookPage(Mode mode, Tabs tab, QWidget *parent) :
     switch(tab)
     {
     case SendingTab:
-        ui->labelExplanation->setVisible(false);
         ui->deleteButton->setVisible(true);
         ui->signMessage->setVisible(false);
         break;
