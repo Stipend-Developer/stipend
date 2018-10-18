84d79f87a20068c25b622980c423e3cef02f6a51
Stipend-Developer <nicksr987@gmail.com>
2018-10-17 21:29:15 -0400

    Fix syntax error w/ masternode.cpp

--- src/masternode/masternode.cpp
+++ src/masternode/masternode.cpp
@@ -243,7 +243,7 @@ void CMasternode::Check()
                 }
             }
         }
-        LogPrintf("Check(): Resetting nLastCheck until next call\n")
+        LogPrintf("Check(): Resetting nLastCheck until next call\n");
         nLastCheck = 0;
     } else {
         nLastCheck += 1;
