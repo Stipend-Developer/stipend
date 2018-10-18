a77f50486c6451e8c8d8cee86d8328d19c2e0094
Stipend-Developer <nicksr987@gmail.com>
2018-10-17 21:56:05 -0400

    Increase number of MN confirms to 1000

--- src/masternode/masternode.h
+++ src/masternode/masternode.h
@@ -28,7 +28,7 @@ class uint256;
 #define MASTERNODE_SYNC_IN_PROCESS             8
 #define MASTERNODE_REMOTELY_ENABLED            9
 
-#define MASTERNODE_MIN_CONFIRMATIONS           600
+#define MASTERNODE_MIN_CONFIRMATIONS           1000
 #define MASTERNODE_MIN_DSEEP_SECONDS           (30*60)
 #define MASTERNODE_MIN_DSEE_SECONDS            (5*60)
 #define MASTERNODE_PING_SECONDS                (1*60)
@@ -171,7 +171,7 @@ public:
                 READWRITE(nScanningErrorCount);
                 READWRITE(nLastScanningErrorBlockHeight);
                 READWRITE(nLastPaid);
-                READWRITE(nLastCheck); 
+                READWRITE(nLastCheck);
         }
     )
 
